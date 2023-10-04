27e9d763c8e95c80a511eaf5a897586f6947c362
Gabriel Orozco <juan.orozco@sourcemeridian.com>
2023-10-04 11:16:12 -0500

    Some assertions were wrote

--- utilities/asserts.py
+++ utilities/asserts.py
@@ -7,3 +7,21 @@ class Asserts:
 
     def was_element_found(self, element, result):
         assert result, f"{element} was not found."
+
+
+    def were_fields_filled(self, result):
+        assert result, "fields were not filled."
+
+
+    def was_form_submitted(self, result):
+        assert result, "form was submitted."
+
+
+    def was_message_displayed_correct(self, result):
+        assert result, "message displayed is not the expected."
+
+
+    def was_file_attached(self, result):
+        assert result, "file was not attached successfully."
+
+
