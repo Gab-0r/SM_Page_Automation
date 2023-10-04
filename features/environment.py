6b80e60bfd41d69215e3014dd27075acf0637d72
Gabriel Orozco <juan.orozco@sourcemeridian.com>
2023-10-04 16:54:36 -0500

    headless mode option implemented

--- features/environment.py
+++ features/environment.py
@@ -3,6 +3,6 @@ from utilities.web_driver import WebDriverManager
 
 
 def before_scenario(context, scenario):
-    web_driver = WebDriverManager("Chrome").create_driver()
-    web_driver.maximize_window()
+    web_driver = WebDriverManager("Chrome headless").create_driver()
+    #web_driver.maximize_window()
     context.browser_interactions = BrowserInteractions(web_driver, 10)
